3f0fe54a56897b455858228f5d1abac1719ca13f
Jorge Langa <jorge.langa.arranz@gmail.com>
2019-09-24 15:04:42 +0200

    Brought to the forefront the classes

---
+++ pyrollinghash/__init__.py
@@ -0,0 +1,6 @@
+# from .alder32 import Adler32
+from .adler32 import Adler32
+from .cyclichash import CyclicHash
+from .threewisehash import ThreeWiseHash
+from .rabinkarphash import KarpRabinHash
+from .generalhash import GeneralHash
